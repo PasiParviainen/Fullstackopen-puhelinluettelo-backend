e95a02e37bd4964aa0aa5778842a8fd34bde6106
PasiParviainen <44867@edu.raseko.fi>
2024-10-07 15:27:04 +0300

    added database support to the app

---
+++ models/contact.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose")
+
+mongoose.set("strictQuery", false)
+
+const url = process.env.MONGODB_URI
+
+console.log("Connecting to", url)
+
+mongoose.connect(url)
+.then(result => {
+    console.log("Connected to MongoDB")
+    console.log("---------------------------------------------")
+})
+.catch((error) => {
+    console.log("Error connecting to MongoDB:", error.message)
+})
+
+const contactSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        minlength: 3,
+        required: true,
+    },
+    number: {
+        type: String,
+        required: true,
+    },
+})
+
+contactSchema.set("toJSON", {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model("Contact", contactSchema)
