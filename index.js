1a4abced76c034aad6e75996ebb14e6d65c86482
PasiParviainen <44867@edu.raseko.fi>
2024-10-08 15:27:21 +0300

    Installed eslint and fixed formatting errors

--- index.js
+++ index.js
@@ -1,111 +1,122 @@
-require("dotenv").config()
-const express = require("express")
-const morgan = require("morgan")
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
 const app = express()
-const cors = require("cors")
-const Contact = require("./models/contact")
+const cors = require('cors')
+const Contact = require('./models/contact')
 
-morgan.token("reqbody", request => {
-    return JSON.stringify(request.body)
+morgan.token('reqbody', request => {
+  return JSON.stringify(request.body)
 })
 
-app.use(express.static("dist"))
+app.use(express.static('dist'))
 
 const errorHandler = (error, request, response, next) => {
-    console.log("----------------------------------")
-    console.error("ERROR", error.message)
-    console.log("----------------------------------")
+  console.log('----------------------------------')
+  console.error('ERROR', error.message)
+  console.log('----------------------------------')
 
-    if (error.name === "CastError") {
-        return response.status(400).send({error: "malformatted id"})
-    } else if (error.name === "ValidationError") {
-        return response.status(400).json({error: error.message})
-    }
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 
 app.use(cors())
 app.use(express.json())
-app.use(morgan(":method :url :status :response-time ms :reqbody"))
+app.use(morgan(':method :url :status :response-time ms :reqbody'))
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({error: 'unknown endpoint'})
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
-let persons = []
-
-app.get("/info", (request, response) => {
-    response.send(`Puheleinluettelolla on ${persons.length} ihmisen tiedot<br>${Date()}`)
-})
-
-app.get("/api/persons", (request, response, next) => {
-    Contact.find({})
+//Get the info page
+app.get('/info', (request, response) => {
+  //Initialize persons variable
+  let persons
+  //Initialize date variable
+  const date = new Date()
+  Contact.find({})
     .then(contacts => {
-        response.json(contacts)
-        persons = contacts
+      //Set persons variable to have the length of contacts array
+      persons = String(contacts.length)
+      //Respond with data to display on the info page
+      response.send(`The phonebook contains ${persons} contacts.<br>
+        Date: ${date.toLocaleDateString()}<br>
+        Time: ${date.getHours()}:${date.getMinutes()}`)
+    })
+})
+
+//Get full list of contacts in the phonebook
+app.get('/api/persons', (request, response, next) => {
+  Contact.find({})
+    .then(contacts => {
+      response.json(contacts)
     })
     .catch(error => next(error))
 })
 
-app.get("/api/persons/:id", (request, response, next) => {
-    Contact.findById(request.params.id)
+//Get contact by ID
+app.get('/api/persons/:id', (request, response, next) => {
+  Contact.findById(request.params.id)
     .then(person => {
-        if (person) {
-            response.json(person)
-        } else {
-            response.status(404).end()
-        }
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
     })
     .catch(error => next(error))
 })
 
-app.delete("/api/persons/:id", (request, response, next) => {
-    Contact.findByIdAndDelete(request.params.id)
-    .then(result => {
-        response.status(204).end()
+//Delete contact by ID
+app.delete('/api/persons/:id', (request, response, next) => {
+  Contact.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
     })
     .catch(error => next(error))
 })
 
-app.post("/api/persons", (request, response, next) => {
-    const body = request.body
-    
-    const contact = new Contact({
-        name: body.name,
-        number: body.number,
-    })
+//Create new contact
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
 
-    contact.save()
+  const contact = new Contact({
+    name: body.name,
+    number: body.number,
+  })
+
+  contact.save()
     .then(savedContact => {
-        response.json(savedContact)
+      response.json(savedContact)
     })
     .catch(error => next(error))
 })
 
-app.put("/api/persons/:id", (request, response, next) => {
-    //const body = request.body
-    const {name, number} = request.body
+//Replace existing contact
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
 
-    /*const person = {
-        name: body.name,
-        number: body.number,
-    }*/
-
-    Contact.findByIdAndUpdate(
-        request.params.id,
-        {name, number},
-        {new: true, runValidators: true, context: "query"})
+  Contact.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: 'query' })
     .then(updatedContact => {
-        response.json(updatedContact)
+      response.json(updatedContact)
     })
     .catch(error => next(error))
 })
 
+//Use error handlers
 app.use(errorHandler)
 app.use(unknownEndpoint)
 
+//Add port listener
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
--- mongo.js
+++ mongo.js
@@ -1,14 +1,14 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log("-------------------------------------")
-    console.log("Invalid arguments: No password")
-    console.log("-------------------------------------")
-    console.log("Valid arguments:")
-    console.log("PASSWORD to see all contacts")
-    console.log("PASSWORD NAME NUMBER to add a new contact.")
-    console.log("-------------------------------------")
-    process.exit(1)
+  console.log('-------------------------------------')
+  console.log('Invalid arguments: No password')
+  console.log('-------------------------------------')
+  console.log('Valid arguments:')
+  console.log('PASSWORD to see all contacts')
+  console.log('PASSWORD NAME NUMBER to add a new contact.')
+  console.log('-------------------------------------')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -16,52 +16,52 @@ const password = process.argv[2]
 const url =
 `mongodb+srv://basbas:${password}@pbdata.mz4qo.mongodb.net/Phonebook?retryWrites=true&w=majority&appName=PBdata`
 
-mongoose.set("strictQuery", false)
+mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const contactSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+  name: String,
+  number: String,
 })
 
-const Contact = mongoose.model("Contact", contactSchema)
+const Contact = mongoose.model('Contact', contactSchema)
 
 if (process.argv.length === 3) {
-    console.log("-------------------------------------")
-    console.log("Phonebook:")
-    console.log("-------------------------------------")
-    Contact.find({}).then(result => {
-        result.forEach(contact => {
-            console.log(`Name:   ${contact.name}`)
-            console.log(`Number: ${contact.number}`)
-            console.log("-------------------------------------")
-        })
-        mongoose.connection.close()
+  console.log('-------------------------------------')
+  console.log('Phonebook:')
+  console.log('-------------------------------------')
+  Contact.find({}).then(result => {
+    result.forEach(contact => {
+      console.log(`Name:   ${contact.name}`)
+      console.log(`Number: ${contact.number}`)
+      console.log('-------------------------------------')
     })
+    mongoose.connection.close()
+  })
 }
 else if (process.argv.length < 5) {
-    console.log("-------------------------------------")
-    console.log("Invalid arguments: No name or number specified")
-    console.log("-------------------------------------")
-    console.log("Valid arguments:")
-    console.log("PASSWORD to see all contacts")
-    console.log("PASSWORD NAME NUMBER to add a new contact.")
-    console.log("-------------------------------------")
-    mongoose.connection.close()
+  console.log('-------------------------------------')
+  console.log('Invalid arguments: No name or number specified')
+  console.log('-------------------------------------')
+  console.log('Valid arguments:')
+  console.log('PASSWORD to see all contacts')
+  console.log('PASSWORD NAME NUMBER to add a new contact.')
+  console.log('-------------------------------------')
+  mongoose.connection.close()
 }
 else if (process.argv.length === 5){
-    const contact = new Contact({
-        name: process.argv[3],
-        number: process.argv[4],
-    })
-    
-    contact.save().then(result => {
-        console.log("-------------------------------------")
-        console.log(`New contact saved!`)
-        console.log("-------------------------------------")
-        console.log(`Name:   ${result.name}`)
-        console.log(`Number: ${result.number}`)
-        console.log("-------------------------------------")
-        mongoose.connection.close()
-    })
+  const contact = new Contact({
+    name: process.argv[3],
+    number: process.argv[4],
+  })
+
+  contact.save().then(result => {
+    console.log('-------------------------------------')
+    console.log('New contact saved!')
+    console.log('-------------------------------------')
+    console.log(`Name:   ${result.name}`)
+    console.log(`Number: ${result.number}`)
+    console.log('-------------------------------------')
+    mongoose.connection.close()
+  })
 }
